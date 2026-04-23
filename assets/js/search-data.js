acae602c923d3339091fdc6d4beffc2bd7635feb
dsvolpe <103271564+dsvolpe@users.noreply.github.com>
2026-04-23 16:07:20 +0000

    Deploying to gh-pages from @ dsvolpe/dsvolpe.github.io@e40f06e1107d7ea7a6cc294c23ab5dd303420b75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
